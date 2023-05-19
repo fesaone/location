a0bec2771013131a0be8c76f7451f91c41b51238
FESA ONE <133826879+fesaone@users.noreply.github.com>
2023-05-19 12:30:51 +0700

    Update app.restapi.js

--- app.restapi.js
+++ app.restapi.js
@@ -1,6 +1,6 @@
 document.getElementById("app").innerHTML = `
 <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-<script src="https://cdn.jsdelivr.net/gh/fesaone/location@main/app.function.js"></script>
+<script src="https://raw.githubusercontent.com/fesaone/location/main/app.function.js"></script>
 `;
 
 var scriptElement = document.createElement('script');
@@ -8,5 +8,5 @@ scriptElement.src = 'https://unpkg.com/leaflet/dist/leaflet.js';
 document.head.appendChild(scriptElement);
 
 var bodyScriptElement = document.createElement('script');
-bodyScriptElement.src = 'https://cdn.jsdelivr.net/gh/fesaone/location@main/app.function.js';
+bodyScriptElement.src = 'https://raw.githubusercontent.com/fesaone/location/main/app.function.js';
 document.body.appendChild(bodyScriptElement);
