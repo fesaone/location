6f045725e0c66763bacf04475c8464959e2bac54
FESA ONE <133826879+fesaone@users.noreply.github.com>
2023-05-19 13:12:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-fetch('https://raw.githubusercontent.com/fesaone/location/main/api.location.json')
-.then(response => response.json())
-.then(data => {
-  var tokoData = data;
+fetch('https://cdn.jsdelivr.net/gh/fesaone/location/api.location.json')
+  .then(response => response.json())
+  .then(data => {
+    var tokoData = data;
 
-  function showError(elementId) {
-    document.getElementById(elementId).innerHTML = "Aktifkan lokasi.";
-  }
-
-  function getDistance(userLocation, tokoLocation, elementId) {
-    var distance = userLocation.distanceTo(tokoLocation);
-    if (distance < 1000) {
-      document.getElementById(elementId).innerHTML = "Jarak " + distance.toFixed(0) + " meter";
-    } else {
-      distance = distance / 1000;
-      document.getElementById(elementId).innerHTML = "Jarak " + distance.toFixed(1) + " KM";
+    function showError(elementId) {
+      document.getElementById(elementId).innerHTML = "Aktifkan lokasi.";
     }
-  }
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var userLocation = L.latLng(position.coords.latitude, position.coords.longitude);
+    function getDistance(userLocation, tokoLocation, elementId) {
+      var distance = userLocation.distanceTo(tokoLocation);
+      if (distance < 1000) {
+        document.getElementById(elementId).innerHTML = "Jarak " + distance.toFixed(0) + " meter";
+      } else {
+        distance = distance / 1000;
+        document.getElementById(elementId).innerHTML = "Jarak " + distance.toFixed(1) + " KM";
+      }
+    }
 
-      tokoData.forEach(function(toko, index) {
-        var tokoLocation = L.latLng(toko.lat, toko.lng);
-        var elementId = "toko" + (index + 1);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var userLocation = L.latLng(position.coords.latitude, position.coords.longitude);
 
-        getDistance(userLocation, tokoLocation, elementId);
+        tokoData.forEach(function(toko, index) {
+          var tokoLocation = L.latLng(toko.lat, toko.lng);
+          var elementId = "toko" + (index + 1);
+
+          getDistance(userLocation, tokoLocation, elementId);
+        });
+      }, function() {
+        tokoData.forEach(function(toko, index) {
+          var elementId = "toko" + (index + 1);
+          showError(elementId);
+        });
       });
-    }, function() {
+    } else {
       tokoData.forEach(function(toko, index) {
         var elementId = "toko" + (index + 1);
         showError(elementId);
       });
-    });
-  } else {
-    tokoData.forEach(function(toko, index) {
-      var elementId = "toko" + (index + 1);
-      showError(elementId);
-    });
-  }
-})
-.catch(error => {
-  console.log('Terjadi kesalahan:', error);
-});
+    }
+  })
+  .catch(error => {
+    console.log('Terjadi kesalahan:', error);
+  });
